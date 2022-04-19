61ec70d1581be636018595b646fc72e942a66edf
MUGIRANEZA JOHN <52497006+mujohn26@users.noreply.github.com>
2022-04-20 00:04:31 +0200

    initialize homepage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<HomePage/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+ 
---
+++ src/pages/home/index.js
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function HomePage() {
+  return (
+    <div  className="homeWrapper">
+      Welcome to wallet app
+    </div>
+  )
+}
+
+export default HomePage;
